20d8a9b4d2c3e540d85a2af972a92e1c408dc560
Sutharsan <132842657+Sudar2002@users.noreply.github.com>
2023-05-28 06:44:26 -0700

    Add files via upload

---
+++ Project 1.py
@@ -0,0 +1,16 @@
+#print('Hello, World!')
+#person=input('Whats Is Your Name?')
+#print('Hello',person)
+#age=14
+#print(age)
+
+
+Weight='30kg'
+print(Weight)
+
+Height='143cm'
+print(Height)
+
+
+
+
